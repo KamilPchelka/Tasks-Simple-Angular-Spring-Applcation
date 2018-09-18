48e6a9880c2fd233390458c0ea1fcd7ae9acab14
Kamil.Pchelka <kamil.pchelka@gmail.com>
2018-09-18 11:31:09 +0200

    Add arbitrary imports.

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,15 +2,21 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {TasksComponent} from "./components/tasks/tasks.component";
+import {TaskService} from "./services/task.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
